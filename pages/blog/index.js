a9395338fbb9de0012e035209e3b2cfddac54659
ElektronicaExpert <elektronica.expert0@gmail.com>
2025-06-20 19:06:31 +0200

    Add files via upload

---
+++ pages/blog/index.js
@@ -0,0 +1,12 @@
+export default function Blog() {
+  return (
+    <div>
+      <h1>Blog</h1>
+      <ul>
+        <li><a href="#">Beste robotstofzuigers van 2025</a></li>
+        <li><a href="#">Top 5 airco’s voor thuis</a></li>
+        <li><a href="#">Waar op letten bij smart tv’s?</a></li>
+      </ul>
+    </div>
+  );
+}
---
+++ pages/index.js
@@ -0,0 +1,8 @@
+export default function Home() {
+  return (
+    <div>
+      <h1>Welkom bij Elektronica Expert</h1>
+      <p>De beste reviews over robotstofzuigers, airco’s, tv’s en meer!</p>
+    </div>
+  );
+}
